12f15b7518b9543377a9eb1f643092799af1b652
brucephillips <bphillips@ku.edu>
2018-10-16 19:28:21 -0500

    more examples

---
+++ src/main/java/me/brucephillips/threading/sec02/RealHelloWorldRunnable.java
@@ -0,0 +1,32 @@
+package me.brucephillips.threading.sec02;
+
+public class RealHelloWorldRunnable
+{
+	public static class Greeter implements Runnable
+	{
+		private String country;
+		
+		public Greeter(String country)
+		{
+
+			this.country = country;
+
+		}
+
+		public void run()
+		{
+			System.out.println("Hello " + country + "!");
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		String countries[] = { "France", "India", "China", "USA", "Germany" };
+
+		for (String country : countries)
+		{
+			Greeter greeter = new Greeter(country);
+			new Thread(greeter, country + " thread").start();
+		}
+	}
+}
